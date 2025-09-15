83ceb25e96cc04ffe3c0b949e50b7129410f4215
ivyycc <88044664+ivyycc@users.noreply.github.com>
2025-09-15 18:29:51 +0000

    map is showing!!

--- qiskit-event/src/WitsMap.tsx
+++ qiskit-event/src/WitsMap.tsx
@@ -2,40 +2,32 @@ import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 
 const containerStyle = {
   width: "100%",
-  height: "100%",
+  height: "400px", // fixed height to start
 };
 
-const witsCoords = {
-  lat: -26.1912498,
-  lng: 28.0261846,
-};
+const witsCoords = { lat: -26.1912498, lng: 28.0261846 };
 
 export default function WitsMap() {
   const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY as string,
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY! ,
   });
 
+  if (!isLoaded) return <p className="text-center text-yellow-500">Loading map...</p>;
+
   return (
-    <div className="w-full lg:w-1/2 h-80 lg:h-auto rounded-xl overflow-hidden shadow-lg">
-      {isLoaded ? (
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={witsCoords}
-          zoom={15}
-          options={{
-            disableDefaultUI: false,
-            zoomControl: true,
-            fullscreenControl: false,
-            mapTypeControl: false,
-            streetViewControl: false,
-          }}
-        >
-          <Marker position={witsCoords} />
-        </GoogleMap>
-      ) : (
-        <p className="text-center text-yellow-500">Loading map...</p>
-      )}
-    </div>
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={witsCoords}
+      zoom={16}
+      options={{
+        disableDefaultUI: false,
+        zoomControl: true,
+        fullscreenControl: false,
+        mapTypeControl: false,
+        streetViewControl: false,
+      }}
+    >
+      <Marker position={witsCoords} />
+    </GoogleMap>
   );
 }
